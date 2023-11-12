ed33e5f0449525125f688ce7316c5fb8e53c60d0
Mathis <mathis.fullriede@gmail.com>
2023-11-12 15:17:13 +0100

    fix build & npm publishing

--- dist/turnstile.d.ts
+++
@@ -1,6 +0,0 @@
-import { TurnstileConstructorArgs, TurnstileValidateArgs, TurnstileValidationResponse } from "./types";
-export declare class TurnstileVerify {
-    private accessToken;
-    constructor(args: TurnstileConstructorArgs);
-    validate(validationArgs: TurnstileValidateArgs): Promise<TurnstileValidationResponse>;
-}
